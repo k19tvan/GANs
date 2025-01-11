d78b72b497d175f2f162f991dda60ddb62a6e61e
k24tvan <24520073@gm.uit.edu.vn>
2025-01-11 19:31:56 +0700

    change

---
+++ models/model_cnn.py
@@ -0,0 +1,152 @@
+import torch.nn as nn
+import torch
+
+class Generator(nn.Module):
+
+    """ 
+    Tensor Size In After Generator's Layers (no batch_size)
+
+    - initial: (1, latent_dim)
+    - dense: (img_size // 16) * (img_size // 16) * 1024)
+    - view: (1024, img_size // 16, img_size // 16)
+    - deconv1: (512, img_size // 8, img_size // 8)
+    - deconv2: (256, img_size // 4, img_size // 4)
+    - deconv3: (128, img_size // 2, img_size // 2)
+    - deconv4: (3, img_size, img_size) 
+
+    * Output (with batch_size): tensor(1, 3, img_size, img_size)
+
+    """
+
+    def __init__(self, latent_dim, img_size=64):
+        super(Generator, self).__init__()
+        
+        self.latent_dim = latent_dim
+        self.img_size = img_size
+
+        self.dense = nn.Linear(latent_dim, (img_size // 16) * (img_size // 16) * 1024) 
+        """ View """
+
+        self.deconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=512)
+
+        self.deconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_features=256)
+
+        self.deconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(num_features=128)
+
+        self.deconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=3, kernel_size=4, stride=2, padding=1)
+        self.bn4 = nn.BatchNorm2d(num_features=3)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, z):
+        z  = self.dense(z)
+        z = z.view(z.size(0), 1024, self.img_size // 16, self.img_size // 16)
+
+        z = self.relu(self.bn1(self.deconv1(z)))
+        z = self.relu(self.bn2(self.deconv2(z)))
+        z = self.relu(self.bn3(self.deconv3(z)))
+        z = self.deconv4(z)
+
+        z = torch.tanh(z)
+
+        return z
+
+class Discriminator(nn.Module):
+
+    """ 
+    Tensor Size In After Discriminator's Layers (no batch_size)
+
+    - initial: (3, img_size, img_size)
+    - conv1: (128, img_size // 2, img_size // 2)
+    - conv2: (256, img_size // 4, img_size // 4)
+    - conv3: (512, img_size // 8, img_size // 8)
+    - conv4: (1024, img_size // 16, img_size / 1)
+    - gap: (1024)
+    - tanh: (1024)
+    
+    * Output (with bath_size): tensor(1, 1024)
+    
+    """
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(512)
+
+        self.conv4 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1)
+        self.bn4 = nn.BatchNorm2d(1024)
+
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = nn.Linear(1024, 1)
+
+        self.lkrelu = nn.LeakyReLU(0.2)
+        
+    def forward(self, img):
+        img = self.lkrelu(self.bn1(self.conv1(img)))
+        img = self.lkrelu(self.bn2(self.conv2(img)))
+        img = self.lkrelu(self.bn3(self.conv3(img)))
+        img = self.lkrelu(self.bn4(self.conv4(img)))
+
+        img = torch.mean(img, dim=(2, 3))
+        img = self.linear(img)
+        img = torch.sigmoid(img)
+
+        return img
+
+# class Discriminator(nn.Module):
+
+#     """ 
+#     Tensor Size In After Discriminator's Layers (no batch_size)
+
+#     - initial: (3, img_size, img_size)
+#     - conv1: (128, img_size // 2, img_size // 2)
+#     - conv2: (256, img_size // 4, img_size // 4)
+#     - conv3: (512, img_size // 8, img_size // 8)
+#     - conv4: (1024, img_size // 16, img_size / 1)
+#     - gap: (1024)
+#     - tanh: (1024)
+    
+#     * Output (with bath_size): tensor(1, 1024)
+    
+#     """
+
+#     def __init__(self):
+#         super(Discriminator, self).__init__()
+
+#         self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=2, padding=1)
+#         self.bn1 = nn.BatchNorm2d(128)
+
+#         self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1)
+#         self.bn2 = nn.BatchNorm2d(256)
+
+#         self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1)
+#         self.bn3 = nn.BatchNorm2d(512)
+
+#         self.conv4 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1)
+#         self.bn4 = nn.BatchNorm2d(1024)
+
+#         self.conv5 = nn.Conv2d(in_channels=1024, out_channels=1, kernel_size=4, stride=1, padding=0)
+
+#         self.lkrelu = nn.LeakyReLU(0.2)
+        
+#     def forward(self, img):
+#         img = self.lkrelu(self.bn1(self.conv1(img)))
+#         img = self.lkrelu(self.bn2(self.conv2(img)))
+#         img = self.lkrelu(self.bn3(self.conv3(img)))
+#         img = self.lkrelu(self.bn4(self.conv4(img)))
+#         img = self.conv5(img)
+        
+#         img = torch.sigmoid(img)
+#         print(img.shape)
+
+#         return img
--- train.py
+++ train.py
@@ -4,18 +4,43 @@ import torch.optim.adam
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 from pathlib import Path
-from assets.utils import mean, std, imshow
 from assets.customdatasets import CustomDataset
-from assets.model import Discriminator, Generator
+from models.model_cnn import Discriminator, Generator
 import matplotlib.pyplot as plt
 import torchvision
-import numpy as np
 import argparse
 from tqdm import tqdm
+import cv2
+from pathlib import Path
+import copy 
 
 """ PipeLine: Hyperparameters -> Transforms -> Dataloader -> Model -> Training """
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# mean = torch.tensor([0.485, 0.456, 0.406])
+# std = torch.tensor([0.229, 0.224, 0.225])
+
+mean = torch.tensor([0.5, 0.5, 0.5])    
+std = torch.tensor([0.5, 0.5, 0.5])
+
+def imshow(image):
+
+    device = image.device
+
+    cur_mean = mean.view(-1, 1, 1)
+    cur_std = std.view(-1, 1, 1)
+
+    img = copy.deepcopy(image)
+    img = img.mul(cur_std.to(device)).add(cur_mean.to(device))
+    img = img.clamp(0, 1)
+
+    img = img.permute(1, 2, 0).cpu().numpy()
+
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
 
 """ Hyper Parameter """
 
@@ -29,8 +54,9 @@ def parse_args():
     parser.add_argument('--lr', type=float, default=0.0002)
     parser.add_argument('--beta1', type=float, default='0.5')
     parser.add_argument('--beta2', type=float, default='0.999')
-    parser.add_argument('--latent_dim', type=int, default=100)
-    parser.add_argument('--img_size', type=int, default=128)
+    parser.add_argument('--latent_dim', type=int, default=70)
+    parser.add_argument('--img_size', type=int, default=64)
+    parser.add_argument('--save_period', type=int, default=1)
     
     return parser.parse_args()
 
@@ -44,12 +70,12 @@ beta1 = args.beta1
 beta2 = args.beta2
 latent_dim = args.latent_dim
 img_size = args.img_size
+save_period = args.save_period
 
 """ Transforms """
 
 transform = transforms.Compose([
     transforms.Resize(img_size),
-    transforms.CenterCrop(img_size),
     transforms.ToTensor(),
     transforms.Normalize(mean, std)
 ])
@@ -70,7 +96,7 @@ for i, image in enumerate(images):
 """ Model """
 
 generator = Generator(latent_dim, img_size=img_size).to(device)
-discriminator = Discriminator(img_size=img_size).to(device)
+discriminator = Discriminator().to(device)
 
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=lr, betas=(beta1, beta2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(beta1, beta2))
@@ -79,26 +105,51 @@ adversarial_loss = nn.BCELoss()
 
 """ Training """
 
+def count_files(dir):
+    return len(list(dir.glob("*")))
+
 def training():
+
+    run_dir = Path("run")
+    run_dir.mkdir(parents=True, exist_ok=True)
+
+    cnt = count_files(run_dir)
+
     for epoch in range(num_epochs):
         with tqdm(total=len(dataloader), desc=f"Epoch {epoch+1}/{num_epochs}") as pbar:
             for i, imgs in enumerate(dataloader):
 
                 real_imgs = imgs.to(device)
                 batch_size = real_imgs.size(0)
+
                 valid = torch.ones(batch_size, 1).to(device)
                 fake = torch.zeros(batch_size, 1).to(device)
 
+                """ Generator Training """
                 optimizer_D.zero_grad()
+                z = torch.randn(batch_size, latent_dim).to(device)
+                fake_imgs = generator(z).to(device).detach()
+
+                # print(valid.shape)
+
                 real_loss = adversarial_loss(discriminator(real_imgs), valid)
-                fake_loss = adversarial_loss(discriminator(generator(torch.randn(batch_size, 100).to(device)).detach()), fake)
+                fake_loss = adversarial_loss(discriminator(fake_imgs), fake)
+                
                 d_loss = (real_loss + fake_loss) / 2
                 d_loss.backward()
+
                 optimizer_D.step()
 
+                """ Discriminator Traning"""
                 optimizer_G.zero_grad()
-                g_loss = adversarial_loss(discriminator(generator(torch.randn(batch_size, 100).to(device))), valid)
+
+                z = torch.randn(batch_size, latent_dim).to(device)
+                fake_imgs = generator(z).to(device)
+
+                g_loss = adversarial_loss(discriminator(fake_imgs), valid)
+
                 g_loss.backward()
+
                 optimizer_G.step()
 
                 pbar.update(1)
@@ -109,15 +160,24 @@ def training():
                         g_loss=g_loss.item()
                     )
 
-            if epoch + 1 == num_epochs: 
+            if (epoch + 1) % save_period == 0:
                 with torch.no_grad():
-                    z = torch.randn(16, 100).to(device)
+                    z = torch.randn(16, latent_dim).to(device)
+
                     generated_imgs = generator(z).detach().cpu()
+                    generated_imgs = (generated_imgs + 1) / 2
+
                     grid = torchvision.utils.make_grid(generated_imgs, nrow=4, normalize=True)
-                    plt.imshow(np.transpose(grid, (1, 2, 0)))
-                    plt.title(f"Epoch {epoch}")
-                    plt.axis('off')
-                    plt.show()
+
+                    img = grid.permute(1, 2, 0).numpy() * 255
+
+                    cur_dir = run_dir/Path(f"run{cnt + 1}")
+                    cur_dir.mkdir(parents=True, exist_ok=True)
+
+                    image_dir = cur_dir/Path(f"Epoch{epoch + 1}.png")
+
+                    print(f"Save image at {image_dir}")
+                    cv2.imwrite(image_dir, img)
 
 if __name__ == '__main__':
 
@@ -131,6 +191,7 @@ if __name__ == '__main__':
     print(f"beta2 = {beta2}")
     print(f"latent_dim = {latent_dim}")
     print(f"img_size = {img_size}")
+    print(f"save_period = {save_period}")
 
     print()
 
